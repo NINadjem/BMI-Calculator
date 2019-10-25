a72674ef6d89b7e59ade54b424dc66de73f2cf09
Nadjem Nour El Imane <47980833+NINadjem@users.noreply.github.com>
2019-10-25 01:34:44 +0100

    *

--- BMICalculator/app/src/main/java/com/nadjemni/bmicalculator/MainActivity.kt
+++ BMICalculator/app/src/main/java/com/nadjemni/bmicalculator/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.nadjemni.bmicalculator
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calculateBtn.setOnClickListener {
+            if(heightEDTX.text.isNotEmpty() && weightEDTX.text.isNotEmpty()) {
+                val weight=weightEDTX.text.toString().toDouble()
+                val height=heightEDTX.text.toString().toInt()
+                if(weight>0&&weight<600&&height>=50&&height<2.5){
+                val intent = Intent(this@MainActivity,  ResultActivity::class.java)
+                intent.putExtra("bmi", calculateBMI(weight,height))
+                startActivity(intent)
+                }else{
+                showErrorSnack("Incorrect Values")
+                }
+            }else{
+                showErrorSnack("A filed is missing")
+            }
+        }
     }
+
+    @SuppressLint("ResourceAsColor")
+    private fun showErrorSnack(errorMsg:String) {
+        val snackbar = Snackbar.make(container, "error : $errorMsg !", Snackbar.LENGTH_LONG)
+        snackbar.view.setBackgroundColor(R.color.colorRed)
+        snackbar.show()
+    }
+
+   private fun calculateBMI(weight:Double,height:Int)=(weight/((height/100)*(height/100)))
+
 }
---
+++ BMICalculator/app/src/main/java/com/nadjemni/bmicalculator/ResultActivity.kt
@@ -0,0 +1,50 @@
+package com.nadjemni.bmicalculator
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        skipResultBTN.setOnClickListener {
+            startActivity( Intent(this@ResultActivity,  MainActivity::class.java))
+        }
+        val bmi = intent.getDoubleExtra("bmi", -1.0)
+        if (bmi == -1.0) {
+            containerL.visibility= View.GONE
+        } else {
+            bmiValueTV.text=bmi.toString()
+                if (bmi < 18.5) {
+                    containerL.setBackgroundColor(R.color.colorYellow)
+                    bmiFlagImgView.setImageResource(R.drawable.exclamationMark)
+                    bmiLabelTV.text="You have an UnderWeight!"
+                    commentTV.text="Here are some advices To help you increase your weight"
+                    advice2IMG.setImageResource(R.drawable.nowater)
+                    advice1TV.text="Don't drink water before meals"
+                    advice2IMG.setImageResource(R.drawable.bigmeal)
+                    advice2TV.text="Use bigger plates"
+                    advice3TV.text="Get quality sleep"
+
+                } else {
+                    if (bmi > 25) {
+                        containerL.setBackgroundColor(R.color.colorRed)
+                        bmiFlagImgView.setImageResource(R.drawable.exclamationMark)
+                        bmiLabelTV.text="You have an OverWeight!"
+                        commentTV.text="Here are some advices To help you decrease your weight"
+                        advice2IMG.setImageResource(R.drawable.water)
+                        advice1TV.text="Drink water a half hour before meals"
+                        advice2IMG.setImageResource(R.drawable.twoeggs)
+                        advice2TV.text="Eeat only two meals per day and make sure that they contains a high protein"
+                        advice3IMG.setImageResource(R.drawable.nosugar)
+                        advice3TV.text="Drink coffee or tea and Avoid sugary food"
+                    }
+                }
+            }
+        }
+}
