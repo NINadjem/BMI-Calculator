9ba5c0f4b2e7c3064225699bc0012a78557c1109
Nadjem Nour El Imane <47980833+NINadjem@users.noreply.github.com>
2019-10-25 02:10:23 +0100

    *

--- BMICalculator/app/src/main/java/com/nadjemni/bmicalculator/MainActivity.kt
+++ BMICalculator/app/src/main/java/com/nadjemni/bmicalculator/MainActivity.kt
@@ -14,30 +14,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        actionBar?.setIcon(R.drawable.logo)
         calculateBtn.setOnClickListener {
-            if(heightEDTX.text.isNotEmpty() && weightEDTX.text.isNotEmpty()) {
-                val weight=weightEDTX.text.toString().toDouble()
-                val height=heightEDTX.text.toString().toInt()
-                if(weight>0&&weight<600&&height>=50&&height<2.5){
-                val intent = Intent(this@MainActivity,  ResultActivity::class.java)
-                intent.putExtra("bmi", calculateBMI(weight,height))
-                startActivity(intent)
-                }else{
-                showErrorSnack("Incorrect Values")
+            if (heightEDTX.text.isNotEmpty() && weightEDTX.text.isNotEmpty()) {
+                val weight = weightEDTX.text.toString().toDouble()
+                val height = heightEDTX.text.toString().toInt()
+                if (weight > 0 && weight < 600 && height >= 50 && height < 2.5) {
+                    val intent = Intent(this@MainActivity, ResultActivity::class.java)
+                    intent.putExtra("bmi", calculateBMI(weight, height))
+                    startActivity(intent)
+                } else {
+                    showErrorSnack("Incorrect Values")
                 }
-            }else{
+            } else {
                 showErrorSnack("A filed is missing")
             }
         }
     }
 
     @SuppressLint("ResourceAsColor")
-    private fun showErrorSnack(errorMsg:String) {
+    private fun showErrorSnack(errorMsg: String) {
         val snackbar = Snackbar.make(container, "error : $errorMsg !", Snackbar.LENGTH_LONG)
         snackbar.view.setBackgroundColor(R.color.colorRed)
         snackbar.show()
     }
 
-   private fun calculateBMI(weight:Double,height:Int)=(weight/((height/100)*(height/100)))
+    private fun calculateBMI(weight: Double, height: Int) =
+        (weight / ((height / 100) * (height / 100)))
 
 }
